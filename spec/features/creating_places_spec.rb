834d6d6472139c628bb9dbb6d8c5f957707c7aab
marvin <marvkipi@gmail.com>
2015-11-02 19:03:06 -0800

    gem bootstrap-sass, font-awesome, simple-form

---
+++ spec/features/creating_places_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.feature "Users can create new place" do 
+	scenario "with valid attributes" do 
+		visit "/"
+
+		click_link "New Place"
+
+		fill_in "Name", with: "Powell"
+		fill_in "Address", with: "On Burnside"
+		fill_in "Event", with: "Homeless gathering"
+		click_button "Create Place"
+
+		expect(page).to have_content "A Place has been created."
+	end
+end
